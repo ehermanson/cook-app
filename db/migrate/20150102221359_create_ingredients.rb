0231c0fcf4cd93e633b3e3050497b3f84f92bade
Eric <eric.r.hermanson@gmail.com>
2015-01-06 18:56:25 -0500

    clean up migrations

--- db/migrate/20150102221359_create_ingredients.rb
+++ db/migrate/20150102221359_create_ingredients.rb
@@ -2,8 +2,6 @@ class CreateIngredients < ActiveRecord::Migration
   def change
     create_table :ingredients do |t|
       t.string :name
-      t.integer :quantity
-      t.string :unit
       t.belongs_to :recipe, index: true
 
       t.timestamps
--- db/migrate/20150105032414_remove_quantity_unitfrom_ingredients.rb
+++
@@ -1,6 +0,0 @@
-class RemoveQuantityUnitfromIngredients < ActiveRecord::Migration
-  def change
-  	remove_column :ingredients, :quantity
-  	remove_column :ingredients, :unit
-  end
-end
