56df3802a4602f1dad4a3f829af716e78ad81b13
Eric <eric.r.hermanson@gmail.com>
2015-01-06 18:53:22 -0500

    jquery turbolinks, category nav update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,15 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require cocoon
 //= require turbolinks
 //= require_tree .
-
-$(document).ready(function(){
-	$('.toggle-trigger').on('click', function () {
-		console.log('clicked');
-		$(this).toggleClass('active').next('.toggle-content').slideToggle();
-	})
-})
