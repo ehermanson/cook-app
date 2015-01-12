1b1276d75f73129e7aa0bda4624daa43620cbe84
Eric <eric.r.hermanson@gmail.com>
2015-01-12 17:47:25 -0500

    default user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,7 @@ class User < ActiveRecord::Base
 
 	has_many :recipes
 
-	has_attached_file :avatar,
-										:styles => { :medium => "300x300>", :thumb => "100x100>" },
-										:default_url => "/assets/images/missing_:style.jpg"
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing_:style.jpg"
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
